42492ec7f91db1846c717c48da68db7206a0fae1
niuxinghua <niuxinghua@haier.com>
2018-06-04 17:47:54 +0800

    no message
    Change-Id: I079b81e66d5851fd9ee53e5c7887b89af2f828a1

---
+++ module-demo2/subproject/sourcefile/Add4.h
@@ -0,0 +1,12 @@
+//
+//  Add4.h
+//  Masonry
+//
+//  Created by niuxinghua on 2018/6/4.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Add4 : NSObject
+
+@end
---
+++ module-demo2/subproject/sourcefile/Add4.m
@@ -0,0 +1,12 @@
+//
+//  Add4.m
+//  Masonry
+//
+//  Created by niuxinghua on 2018/6/4.
+//
+
+#import "Add4.h"
+
+@implementation Add4
+
+@end
