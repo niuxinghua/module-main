563db07fdf83c0f98774e54f71173794a70970cc
niuxinghua <niuxinghua@haier.com>
2018-06-04 14:02:14 +0800

    main add module2
    Change-Id: I761722d7a8660ec95d5c299092cb07b8c500ac6e

---
+++ module-main/module-demo2/subproject/sourcefile/MainAddModule2.h
@@ -0,0 +1,12 @@
+//
+//  MainAddModule2.h
+//  Masonry
+//
+//  Created by niuxinghua on 2018/6/4.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MainAddModule2 : NSObject
+
+@end
---
+++ module-main/module-demo2/subproject/sourcefile/MainAddModule2.m
@@ -0,0 +1,12 @@
+//
+//  MainAddModule2.m
+//  Masonry
+//
+//  Created by niuxinghua on 2018/6/4.
+//
+
+#import "MainAddModule2.h"
+
+@implementation MainAddModule2
+
+@end
