23b5b3c722972bffa21189d2c11b2d0ba6ab60c1
niuxinghua <niuxinghua@haier.com>
2018-06-04 10:55:55 +0800

    init
    Change-Id: I9129af65b25269d2063b51a835be765c5b9847e0

---
+++ module-main/module-main/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  module-main
+//
+//  Created by niuxinghua on 2018/6/4.
+//  Copyright © 2018年 com.haier. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
