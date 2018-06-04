a3aa63ca86a3c38e53b8cbcf75836da60e1c2f61
niuxinghua <niuxinghua@haier.com>
2018-06-04 14:45:48 +0800

    no message
    Change-Id: Ic2b6566a332ecc82f3d3d842409a3ffd84702441

---
+++ module-main/module-demo2/subproject/sourcefile/AddfromMain3.h
@@ -0,0 +1,12 @@
+//
+//  AddfromMain3.h
+//  Masonry
+//
+//  Created by niuxinghua on 2018/6/4.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AddfromMain3 : NSObject
+
+@end
---
+++ module-main/module-demo2/subproject/sourcefile/AddfromMain3.m
@@ -0,0 +1,12 @@
+//
+//  AddfromMain3.m
+//  Masonry
+//
+//  Created by niuxinghua on 2018/6/4.
+//
+
+#import "AddfromMain3.h"
+
+@implementation AddfromMain3
+
+@end
